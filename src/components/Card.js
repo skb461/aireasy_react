3202b5fdf41b72bbf4f93648e2ee47940aba6002
skb461 <53707185+skb461@users.noreply.github.com>
2022-05-02 02:51:54 +0600

    Delete src/components directory

--- src/components/Card.js
+++
@@ -1,8 +0,0 @@
-import './Card.css';
-
-function Card(props) {
-    const classes = 'card_expance ' + props.className;
-    return <div className={classes}>{props.children}</div>;
-}
-
-export default Card;
