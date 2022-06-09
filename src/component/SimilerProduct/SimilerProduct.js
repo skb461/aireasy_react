1b42b95f704befc66e0d74dfdde8223f409e88f6
skb461 <53707185+skb461@users.noreply.github.com>
2022-06-09 23:37:56 +0600

    Add files via upload

--- src/component/SimilerProduct/SimilerProduct.js
+++ src/component/SimilerProduct/SimilerProduct.js
@@ -11,7 +11,7 @@ function SimilerProduct() {
             <Card className="mt-3 px-2 displayCard_4 mx-4">
                 <Row className="mt-3">
 
-                    <Col lg={4}>
+                    <Col lg={3}>
                         <Card border="light" className="m-1 rounded border-0">
                             <Card.Body>
                                 <Card.Img variant="top" src={AirEasyMobile} className="d-block mx-auto w-50 mt-5 mb-1"/>
@@ -19,30 +19,26 @@ function SimilerProduct() {
                         </Card>
                     </Col>
 
-                    <Col lg={8}>
-                        <Card border="light border-0" >
+                    <Col lg={6}>
+                        <Card border="light border-0 py-5" >
                             <Card.Body className="">
                                 <Card.Title>
                                     <h1>Best Home: 1Bd/1Ba and Steps to downtown MV</h1>
                                     <small>Account: Samszuo@gmail.com</small>
                                 </Card.Title>
-                                <Card.Text className="py-2">
-                                    {/* <h1 className="h-1 boldText">lorem dummy text as wish as can</h1> */}
-                                    <Row>
-                                        <Col md={6}>
-                                            <Card.Link href="#" className="btn btn-primary h1">Annual Occupancy</Card.Link>
-                                            <p className="display-1 p-3 bolder text-muted border text-center m-4">55%</p>
-                                        </Col>
-                                        <Col md={6}>
-                                            <Card.Link href="#" className="btn btn-primary h1">Annual Revenue</Card.Link>
-                                            <p className="display-1 p-3 bolder text-muted border text-center m-4">24161USD</p>
-                                        </Col>
-                                    </Row>
-                                </Card.Text>
                             </Card.Body>
-                            {/* <Card.Footer>
-                                <Card.Link href="#" className="btn Button">ABCD</Card.Link>
-                            </Card.Footer> */}
+                        </Card>
+                    </Col>
+                    <Col lg={3}>
+                        <Card border="light border-0 py-5" >
+                            <Card.Body className="">
+                                <Card.Title>
+                                    <h3>Annual Occupancy</h3>
+                                    <h1>55%</h1>
+                                    <h3>Annual Revenue</h3>
+                                    <h1>24161 USD</h1>
+                                </Card.Title>
+                            </Card.Body>
                         </Card>
                     </Col>
                 </Row>
